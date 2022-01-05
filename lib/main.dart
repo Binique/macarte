bde4911c03d5f6cdd7a532c34cf8b774da661355
Leonel Binique <leonel.binique@gmail.com>
2022-01-05 10:07:25 +0100

    Mise à jour main.dart!

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+        body: SafeArea (
+            child:
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.red,
+                  height: 600,
+                  width: 100,
+
+                ),
+
+                Container(child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      color: Colors.yellow,
+                      height: 100,
+                      width: 100,
+
+                  ),
+                    Container(
+                      color: Colors.green,
+                      height: 100,
+                      width: 100,
+
+                    ),
+
+                  ],
+                ),
+                 height: 600,
+                  width: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 600,
+                  width: 100,
+                ),
+              ],
+
+            ),
+          ),
+        ),
+      );
+  }
 }
